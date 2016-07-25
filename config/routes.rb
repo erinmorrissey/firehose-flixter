d6caaeced89b1932e5d05906a66645360fbfdbe4
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-25 21:45:23 +0000

    Create 'courses' controller, build out courses#index view

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Flixter::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :courses, only: [:index, :show]
 
   # Example resource route with options:
   #   resources :products do
