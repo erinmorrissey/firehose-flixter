78d63ac1c9e54b82462de80b744c2e6e7c255acd
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-27 00:41:26 +0000

    Lock down instructor/courses#show page to logged-in course user, refactored code in 'courses' controller & 'show' view

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,8 +1,12 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
-    @course = Course.find(params[:id])
+    # @course = Course.find(params[:id])
+    #    ^ removed b/c we refactored @course into .current_course
+    #      and now the view calls .current_course, so having it here
+    #      would be redundant
   end
 
   def new
@@ -18,7 +22,20 @@ class Instructor::CoursesController < ApplicationController
     end
   end
 
+
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
