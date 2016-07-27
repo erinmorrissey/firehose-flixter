127e6628404004b039787fc0997c834da7e9e908
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-27 00:28:22 +0000

    Lock down section creation to logged-in course user, refactored code in 'sections' controller & 'new' view

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,16 +1,48 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def new
-    @course = Course.find(params[:course_id])
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
+
   private
+
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: 'Unauthorized', status: :Unauthorized
+    end
+  end
+
+  # declares a helper method to Rails, which will let us call it in our views
+  helper_method :current_course
+
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+    # ^ line above is same as if/end statement below
+    # basically says:
+    #     if we've looked up the current_course beforehand,
+    #     use the value that we looked up previously. If we haven't 
+    #     looked up this course before, go into the database, 
+    #     look it up and also make sure to remember the value in 
+    #     case we need to look it up again later. This technique of storing 
+    #     certain values inside of the memory to reduce the times we have to 
+    #     find a certain value inside the database is called memoization
+
+    # if @current_course == nil
+    #   @current_course = Course.find(params[:course_id])
+    #   @current_course
+    # else
+    #   @current_course
+    # end
+  end
 
   def section_params
     params.require(:section).permit(:title)
