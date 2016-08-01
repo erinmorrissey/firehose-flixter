0fe36002027a5fd2231dc6ba8d1a06229f837ba2
Erin Morrissey <erinmorrissey@gmail.com>
2016-08-01 23:41:28 +0000

    Lock down lessons#show view (must have current_user & user must be enrolled in course)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+
+  # We have access to a lesson, which has access to a section, 
+  # which has access to a course. In order words, something like 
+  # current_lesson.section.course will give you access to a course 
+  # if you have a lesson...
+  def require_authorized_for_current_lesson
+    #if current_lesson.section.course.user != current_user
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must enroll in this course to access the lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
