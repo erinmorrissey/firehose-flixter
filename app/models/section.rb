d3830a1338e4b4a0bdfc44cb916be6af4c74e033
Erin Morrissey <erinmorrissey@gmail.com>
2016-08-03 19:35:10 +0000

    Configure section model to use ranked-model gem for ranking, re-order section display on show pages

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,9 @@
 class Section < ActiveRecord::Base
   belongs_to :course
   has_many :lessons
+
+  # pulls in the functionality of the gem to Section model
+  include RankedModel
+  # Sections belong_to Courses, make the ranker scoped to one Course
+  ranks :row_order, :with_same => :course_id
 end
